9ccdd2ec6f080842f429cade5037fd25064675b7
Neza <112897472+jojefvz@users.noreply.github.com>
2023-02-06 12:45:44 -0600

    Implemented status bar along with photo update.

--- images.py
+++ images.py
@@ -10,12 +10,14 @@ my_img1 = ImageTk.PhotoImage(Image.open("images/Ollin.jpeg"))
 my_img2 = ImageTk.PhotoImage(Image.open("images/hummingbird.jpeg"))
 my_img3 = ImageTk.PhotoImage(Image.open("images/stonequetzalcoatl.jpeg"))
 my_img4 = ImageTk.PhotoImage(Image.open("images/reynadeflores.jpeg"))
-my_img5 = ImageTk.PhotoImage(Image.open("images/tlaloc.jpeg"))
-
-image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+my_img5 = ImageTk.PhotoImage(Image.open("images/hummingbirdcodex.jpeg"))
 
 my_label = Label(image=my_img1)
 my_label.grid(row=0, column=0, columnspan=3)
+
+image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+status = Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
 
 
 def forward(imagenum):
@@ -34,9 +36,9 @@ def forward(imagenum):
     my_label.grid(row=0, column=0, columnspan=3)
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
-
-
-
+    #Update status bar
+    status = Label(root, text="Image " + str(imagenum) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 def back(imagenum):
     global my_label
@@ -55,6 +57,11 @@ def back(imagenum):
     button_back.grid(row=1, column=0)
     button_forward.grid(row=1, column=2)
 
+    #Update status bar
+    status = Label(root, text="Image " + str(imagenum) + " of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+
+
 
 button_back = Button(root, text='<<', command=back, state=DISABLED)
 button_exit = Button(root, text='Exit program', command=root.quit)
@@ -62,8 +69,8 @@ button_forward = Button(root, text='>>', command=lambda: forward(2))
 
 button_back.grid(row=1, column=0)
 button_exit.grid(row=1, column=1)
-button_forward.grid(row=1, column=2)
-
+button_forward.grid(row=1, column=2, pady=10)
+status.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
 root.mainloop()
 
